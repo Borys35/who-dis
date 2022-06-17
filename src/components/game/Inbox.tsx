dd80ab35fc05348587c982edcdd5908671cd47c6
Borys35 <boryskac10@gmail.com>
2022-06-17 13:59:24 +0200

    Added deleting messages from sets

--- src/components/game/Inbox.tsx
+++ src/components/game/Inbox.tsx
@@ -4,17 +4,35 @@ import { Message } from "./Message";
 
 interface Props {
   message: string;
+  onClick?: (e: any) => void;
+  clickable?: boolean;
   class?: string;
 }
 
-const StyledInbox = styled(Message)(({ theme }) => ({
-  borderBottomLeftRadius: "0 !important",
-  backgroundColor: theme?.colors.white,
-  color: theme?.colors.black,
-}));
+const StyledInbox = styled(Message)<Pick<Props, "clickable">>(
+  ({ theme, clickable }) => ({
+    borderBottomLeftRadius: "0 !important",
+    backgroundColor: theme?.colors.white,
+    color: theme?.colors.black,
+
+    "&:hover": clickable
+      ? {
+          filter: "brightness(1.2)",
+        }
+      : undefined,
+  })
+);
 
 const Inbox: Component<Props> = (props) => {
-  return <StyledInbox class={props.class}>{props.message}</StyledInbox>;
+  return (
+    <StyledInbox
+      class={props.class}
+      clickable={props.clickable}
+      onClick={props.onClick}
+    >
+      {props.message}
+    </StyledInbox>
+  );
 };
 
 export default Inbox;
--- src/routes/GameSetSingle.tsx
+++ src/routes/GameSetSingle.tsx
@@ -8,6 +8,7 @@ import {
   Show,
 } from "solid-js";
 import { css, styled } from "solid-styled-components";
+import Modal from "../components/blocks/Modal";
 import Button from "../components/common/Button";
 import Form from "../components/common/Form";
 import Input from "../components/common/Input";
@@ -21,6 +22,8 @@ import { GameSetType } from "../typings";
 interface Props {
   id: string;
 }
+
+type MessageType = "inbox" | "reply";
 
 const StyledForms = styled.div({
   display: "grid",
@@ -38,6 +41,10 @@ const GameSetSingle: Component<Props> = (props) => {
   const [gameSet, setGameSet] = createSignal<GameSetType | null>(null);
   const [inbox, setInbox] = createSignal<string>("");
   const [reply, setReply] = createSignal<string>("");
+  const [toDelete, setToDelete] = createSignal<{
+    type: MessageType;
+    i: number;
+  } | null>(null);
   const router = useRouter();
 
   async function handleAddInbox(e: any) {
@@ -65,6 +72,37 @@ const GameSetSingle: Component<Props> = (props) => {
       .match({ id: props.id });
 
     setReply("");
+  }
+
+  function handleStartDeletingMessage(type: MessageType, i: number) {
+    setToDelete({ type, i });
+  }
+
+  async function handleDeleteMessage() {
+    if (!toDelete()) return;
+
+    const values: { inboxes?: string[]; replies?: string[] } = {};
+
+    if (toDelete()!.type === "inbox") {
+      const messages = [...gameSet()!.inboxes];
+      messages.splice(toDelete()!.i, 1);
+      values.inboxes = messages;
+    } else {
+      const messages = [...gameSet()!.replies];
+      messages.splice(toDelete()!.i, 1);
+      values.replies = messages;
+    }
+
+    await supabase
+      .from<GameSetType>("sets")
+      .update(values)
+      .match({ id: props.id });
+
+    setToDelete(null);
+  }
+
+  function handleCancelDeletingMessage() {
+    setToDelete(null);
   }
 
   onMount(async () => {
@@ -98,6 +136,28 @@ const GameSetSingle: Component<Props> = (props) => {
   return (
     <Layout pageTitle="Choose game set">
       <Show when={gameSet()} fallback={() => <div>Loading</div>}>
+        <Modal open={!!toDelete()}>
+          <h5 class={css({ marginBottom: "1rem" })}>Want to delete?</h5>
+          <p class={css({ marginBottom: "2rem" })}>
+            Are you sure you want to delete "
+            {toDelete()!.type === "inbox"
+              ? gameSet()!.inboxes[toDelete()!.i]
+              : gameSet()!.replies[toDelete()!.i]}
+            " from {toDelete()!.type === "inbox" ? "Inboxes" : "Replies"}?
+          </p>
+          <div
+            class={css({
+              display: "flex",
+              gap: "1rem",
+              justifyContent: "center",
+            })}
+          >
+            <Button onClick={handleCancelDeletingMessage}>Cancel</Button>
+            <Button variant="primary" onClick={handleDeleteMessage}>
+              Delete
+            </Button>
+          </div>
+        </Modal>
         <h1
           class={css({
             margin: "4rem 0 2rem 0",
@@ -124,7 +184,13 @@ const GameSetSingle: Component<Props> = (props) => {
             </Form>
             <StyledMessageGrid>
               <For each={gameSet()!.inboxes}>
-                {(inbox) => <Inbox message={inbox} />}
+                {(inbox, i) => (
+                  <Inbox
+                    message={inbox}
+                    clickable={true}
+                    onClick={() => handleStartDeletingMessage("inbox", i())}
+                  />
+                )}
               </For>
             </StyledMessageGrid>
           </div>
@@ -139,7 +205,14 @@ const GameSetSingle: Component<Props> = (props) => {
             </Form>
             <StyledMessageGrid>
               <For each={gameSet()!.replies}>
-                {(reply) => <Reply message={reply} selected={true} />}
+                {(reply, i) => (
+                  <Reply
+                    message={reply}
+                    selected={true}
+                    clickable={true}
+                    onClick={() => handleStartDeletingMessage("reply", i())}
+                  />
+                )}
               </For>
             </StyledMessageGrid>
           </div>
